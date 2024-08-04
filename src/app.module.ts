5f314bb7c056a8e5418b821aabcb677db348fd73
alulamoke <alulam1926@gmail.com>
2024-08-04 22:01:12 +0300

    authentication implemented using JWT access token and refresh token

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,44 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from 'src/common/guards/auth.guard';
+
+import { ConfigModule } from './config/config.module';
+import { ConfigService } from './config/config.service';
+
+import { UsersModule } from './users/user.module';
+import { User } from './users/entities/user.entity';
+import { EmailVerification } from './users/entities/email-verification.entity';
+import { JwtModule } from '@nestjs/jwt';
+
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule,
+    AuthModule,
+    UsersModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [User, EmailVerification],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
 })
 export class AppModule {}
