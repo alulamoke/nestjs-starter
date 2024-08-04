b2819c3e70272d72f10d5126403d5cc56352ffd8
alulamoke <alulam1926@gmail.com>
2024-08-04 21:58:27 +0300

    auth guard added

---
+++ src/common/guards/auth.guard.ts
@@ -0,0 +1,50 @@
+import { Reflector } from '@nestjs/core';
+import {
+  CanActivate,
+  ExecutionContext,
+  HttpException,
+  HttpStatus,
+  Injectable,
+} from '@nestjs/common';
+import { Request } from 'express';
+import { instanceToPlain } from 'class-transformer';
+
+import { AuthService } from 'src/auth/auth.service';
+import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(
+    private reflector: Reflector,
+    private authservice: AuthService,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+    if (!token) {
+      throw new HttpException('Unauthorized.', HttpStatus.UNAUTHORIZED);
+    }
+
+    try {
+      const payload = await this.authservice.verifyAccessToken(token);
+      request['user'] = instanceToPlain(payload);
+    } catch {
+      throw new HttpException('Invalid token.', HttpStatus.FORBIDDEN);
+    }
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
