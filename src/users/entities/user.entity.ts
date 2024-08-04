25c530a23bdd1cecd512c08868b99b5a2f875980
alulamoke <alulam1926@gmail.com>
2024-08-04 21:55:27 +0300

    user entity created

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,40 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Exclude } from 'class-transformer';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  first_name: string;
+
+  @Column()
+  last_name: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Exclude()
+  @Column()
+  password: string;
+
+  @Exclude()
+  @Column({ nullable: true })
+  refresh_token?: string;
+
+  @Column({ default: false })
+  is_verified: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
