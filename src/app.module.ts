1f03052a4abadef6545b7aad3f1badd3159f3080
alulamoke <alulam1926@gmail.com>
2024-08-06 13:17:06 +0300

    Imported custom modules instead of implementing their configurations directly in the AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,45 +1,21 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-
-import { MulterModule } from '@nestjs/platform-express';
-import { AuthModule } from './auth/auth.module';
 
 import { APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from 'src/common/guards/auth.guard';
 
 import { ConfigModule } from './config/config.module';
-import { ConfigService } from './config/config.service';
-
+import { DatabaseModule } from './database/database.module';
+import { FileUploadModule } from './file-upload/file-upload.module';
+import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/user.module';
-import { User } from './users/entities/user.entity';
-import { EmailVerification } from './users/entities/email-verification.entity';
 
 @Module({
   imports: [
     ConfigModule,
+    DatabaseModule,
+    FileUploadModule,
     AuthModule,
     UsersModule,
-    MulterModule.registerAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        dest: configService.get('LOCAL_FILE_UPLOAD_DEST'),
-      }),
-    }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: +configService.get('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_NAME'),
-        entities: [User, EmailVerification],
-        synchronize: true,
-      }),
-    }),
   ],
   providers: [
     {
