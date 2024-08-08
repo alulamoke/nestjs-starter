3d90e0d4af9007c6777303624623ce22d5fc123f
alulamoke <alulam1926@gmail.com>
2024-08-08 17:35:38 +0300

    fix: import custom modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,34 @@
-import { Module } from '@nestjs/common';
+import { ClassSerializerInterceptor, Module } from '@nestjs/common';
 
-import { APP_GUARD } from '@nestjs/core';
-import { AuthGuard } from 'src/common/guards/auth.guard';
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
+import { AuthGuard } from '@/common/guards/auth.guard';
 
-import { ConfigModule } from './config/config.module';
-import { DatabaseModule } from './database/database.module';
-import { FileUploadModule } from './file-upload/file-upload.module';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from '@/database/database.module';
+import { FileUploadModule } from '@/file-upload/file-upload.module';
+import { AuthModule } from '@/auth/auth.module';
+import { UsersModule } from '@/users/user.module';
+import { SwaggerConfigModule } from '@/swagger-config/swagger-config.module';
 
 @Module({
   imports: [
-    ConfigModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     DatabaseModule,
     FileUploadModule,
     AuthModule,
     UsersModule,
+    SwaggerConfigModule,
   ],
   providers: [
     {
       provide: APP_GUARD,
       useClass: AuthGuard,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ClassSerializerInterceptor,
     },
   ],
 })
