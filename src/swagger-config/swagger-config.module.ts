9cb937415918b58c5784ea958a0da47cfa6d206b
alulamoke <alulam1926@gmail.com>
2024-08-23 22:38:05 +0300

    swagger config update

--- src/swagger-config/swagger-config.module.ts
+++ src/swagger-config/swagger-config.module.ts
@@ -5,22 +5,10 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 export class SwaggerConfigModule {
   static setup(app: INestApplication): void {
     const config = new DocumentBuilder()
-      .setTitle('YegnaCinema API')
+      .setTitle('nestjs-starter API')
       .setDescription(
         `
-      **YegnaCinema** is an online platform that allows users to book cinema tickets conveniently. 
-      
-      With YegnaCinema, you can browse movie listings, select showtimes, and purchase tickets from the comfort of your home or on the go. 
-      Enjoy the latest movies in theaters across Ethiopia without the hassle of long queues.
-      
-      ### Features:
-      - Browse and search for movies currently playing in theaters.
-      - View detailed information about each movie, including synopsis, cast, and ratings.
-      - Check showtimes and availability for multiple cinemas.
-      - Book and purchase tickets securely.
-      - Receive digital tickets via email or SMS.
-      
-      Whether you're planning a movie night with friends or a special date, YegnaCinema makes it easy to enjoy the cinema experience.
+      **nestjs-starter
     `,
       )
       .setVersion('1.0')
